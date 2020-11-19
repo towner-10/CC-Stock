3751002bce9bf99c647cc04afb4239eb6e3f26c0
Collin Town <tntcommando1@gmail.com>
2020-11-19 12:06:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Canada Computers Stock Checker
+
+This script is written in Python 3 and uses the following libraries
+- requests
+- threading
+- BeautifulSoup
+- win10toast_persist
+
+## How to Run
+
+Make sure you have Python3 installed. It can be acquired through installing it on the Windows Store.
+
+Run this script in a program that supports colour code output. I used Windows Terminal which can be acquired on the Windows Store.
+
+Run the script using `python3 <folder>/main.py` and it will then start running and sending desktop notifications when a product is availible in stock. If you don't want to track RTX 3080s, change the URL to the product that you want to track on the search page.
+
+## Upcoming Features
+
+- 📍 Ability to check where it is in stock and set a prefered location.
---
+++ main.py
@@ -0,0 +1,69 @@
+import requests
+import threading
+import json
+from bs4 import BeautifulSoup
+from win10toast_persist import ToastNotifier
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+
+counter = 0
+
+# Chnage URL to other product such as the RTX 3070
+# https://www.canadacomputers.com/index.php?cPath=43&sf=:3_7&mfr=&pr=
+URL = 'https://www.canadacomputers.com/index.php?cPath=43&sf=:3_5&mfr=&pr='
+toaster = ToastNotifier()
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    CLEAR = '\033[H\033[J'
+
+def getData():
+    global counter
+    threading.Timer(30.0, getData).start()
+
+    try:
+        page = requests.get(URL)
+    except:
+        print("Could not connect to: " + URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find_all('div', class_="col-xl-3 col-lg-4 col-6 mt-0_5 px-0_5 toggleBox mb-1")
+
+    print(bcolors.CLEAR, end='')
+
+    itemCount = 0
+
+    for stock in results:
+        stockStatus = stock.find('div').find('div').find('div', class_="allInfoSearch").find('div', class_='row').find('div').find('a').find_all('div')[1].text.strip()
+        stockURL = stock.find('div').find('div').find('div', class_="productImageDesc").find('div', class_='row').find('div', class_='productInfoSearch').find('span').find('a').get('href')
+        stockName = stock.find('div').find('div').find('div', class_="productInfoSearch").find('span', class_='productTemplate_title').find('a').text.strip()
+
+        itemCount += 1
+
+        if (stockStatus != 'In-Store Back Order'):
+            parsed = urlparse.urlparse(stockURL)
+            stockCheckURL = 'https://www.canadacomputers.com/product_info.php?ajaxstock=true&itemid=' + str(parse_qs(parsed.query)['item_id'][0])
+
+            try:
+                stockCheckRequest = json.loads(requests.get(stockCheckURL).text)
+            except:
+                print(bcolors.FAIL + "Error getting stock data!" + bcolors.ENDC)
+
+            toaster.show_toast(stockStatus, stockName, duration=None)
+            print(bcolors.OKGREEN + bcolors.BOLD + stockStatus + bcolors.ENDC + " | " + stockCheckRequest['loc'] + " | " + stockName[:35] + "...")
+        else:
+            print(bcolors.FAIL + bcolors.BOLD + stockStatus + bcolors.ENDC + " | " + stockURL)
+
+    counter += 1
+
+    print('\nLoops: ' + str(counter) + '\t Items this loop: ' + str(itemCount))
+
+getData()
